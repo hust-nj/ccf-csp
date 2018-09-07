92eb9e5f7d589ba83209c0150a291c9e70194880
ninja <2402224349@qq.com>
2018-09-07 09:30:08 +0800

    update

--- 201712-2/main.cpp
+++ 201712-2/main.cpp
@@ -1,9 +1,33 @@
-#include <iostream>
-
+#include<iostream>
+#include<vector>
+#include<algorithm>
 using namespace std;
 
 int main()
 {
-    cout << "Hello world!" << endl;
+    int n, k;
+    cin >> n >> k;
+    vector<bool> v(n, true);
+    int del = 0;
+    int num = 1, cur = 0;
+    while(del != n-1)
+    {
+        cur %= n;
+        if(v[cur] == false)
+            ++cur;
+        else if(num % k == 0 || num % 10 == k)
+        {
+            v[cur] = false;
+            ++del;
+            ++num;
+            ++cur;
+        }
+        else
+        {
+            ++num;
+            ++cur;
+        }
+    }
+    cout << find(v.begin(), v.end(), true) - v.begin() + 1 << endl;
     return 0;
 }
